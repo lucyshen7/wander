ab10b30abe8afecc7db2b62f5d59ae650038fbaa
Lucy Shen <lucyshen77@gmail.com>
2022-03-16 22:05:59 -0700

    add more text fields for AddActivityForm, still need to add transition, style, remaining form fields, and implement post request

--- react-front-end/src/AddActivityForm.tsx
+++ react-front-end/src/AddActivityForm.tsx
@@ -61,12 +61,38 @@ export const AddActivityForm: React.FC<Props> = ({ addActivity, tripId }) => {
         </Button>
       </form> */}
 
+      {/* Change to date selection */}
       <TextField
         id="date-input"
         name="date"
         label="Date"
         type="text"
         value={formValues.date}
+        onChange={handleInputChange}
+      />
+      <TextField
+        id="activityName-input"
+        name="activityName"
+        label="Activity Name"
+        type="text"
+        value={formValues.activityName}
+        onChange={handleInputChange}
+      />
+      <TextField
+        id="activityAddress-input"
+        name="activityAddress"
+        label="Activity Address"
+        type="text"
+        value={formValues.activityAddress}
+        onChange={handleInputChange}
+      />
+      {/* Add dropdown for type of activity */}
+      <TextField
+        id="cost-input"
+        name="cost"
+        label="Cost"
+        type="number"
+        value={formValues.cost}
         onChange={handleInputChange}
       />
     </div>
