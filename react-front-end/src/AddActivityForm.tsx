0014e92046fc99f0b3fa352e96f770b6d790828e
Lucy Shen <lucyshen77@gmail.com>
2022-03-17 01:16:21 -0700

    add form dialog

--- react-front-end/src/AddActivityForm.tsx
+++ react-front-end/src/AddActivityForm.tsx
@@ -11,6 +11,13 @@ import TextField from "@mui/material/TextField";
 // import Slider from "@material-ui/core/Slider";
 // import Button from "@material-ui/core/Button";
 
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+
 interface Props {
   addActivity: AddActivity;
   tripId: number;
@@ -27,7 +34,16 @@ const defaultValues = {
 };
 
 export const AddActivityForm: React.FC<Props> = ({ addActivity, tripId }) => {
+  // const [open, setOpen] = React.useState(false);
   const [formValues, setFormValues] = useState(defaultValues);
+
+  // const handleClickOpen = () => {
+  //   setOpen(true);
+  // };
+
+  // const handleClose = () => {
+  //   setOpen(false);
+  // };
 
   const handleInputChange = (e: any) => {
     const { name, value } = e.target;
@@ -39,6 +55,33 @@ export const AddActivityForm: React.FC<Props> = ({ addActivity, tripId }) => {
 
   return (
     <div className="todo-form">
+      {/* <div>
+        <Button variant="outlined" onClick={handleClickOpen}>
+          Open form dialog
+        </Button>
+        <Dialog open={open} onClose={handleClose}>
+          <DialogTitle>Subscribe</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              To subscribe to this website, please enter your email address
+              here. We will send updates occasionally.
+            </DialogContentText>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="name"
+              label="Email Address"
+              type="email"
+              fullWidth
+              variant="standard"
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>Cancel</Button>
+            <Button onClick={handleClose}>Subscribe</Button>
+          </DialogActions>
+        </Dialog>
+      </div> */}
       {/* <form className="add-todo">
         <input
           type="text"
--- react-front-end/src/ViewTrip.tsx
+++ react-front-end/src/ViewTrip.tsx
@@ -1,7 +1,14 @@
-import { Button } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import "./App.scss";
 import { ViewTripItem } from "./ViewTripItem";
+import TextField from "@mui/material/TextField";
+
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
 
 interface Props {
   trip: Trip["id"];
@@ -10,12 +17,41 @@ interface Props {
   setForm: Function;
 }
 
+const defaultValues = {
+  // an obj initialized with properties for each form value
+  tripId: 0,
+  date: "",
+  activityName: "",
+  activityAddress: "",
+  type: "",
+  cost: 0,
+};
+
 export const ViewTrip: React.FC<Props> = ({
   trip,
   closeView,
   activities,
   setForm,
 }) => {
+  const [open, setOpen] = React.useState(false);
+  const [formValues, setFormValues] = useState(defaultValues);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleInputChange = (e: any) => {
+    const { name, value } = e.target;
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    });
+  };
+
   return (
     <div className="view-trip">
       <div className="trip-header">
@@ -48,6 +84,72 @@ export const ViewTrip: React.FC<Props> = ({
         >
           Add Activity
         </Button>
+
+        <div>
+          <Button variant="outlined" onClick={handleClickOpen}>
+            Open form dialog
+          </Button>
+          <Dialog open={open} onClose={handleClose}>
+            <DialogTitle>Add an Activity</DialogTitle>
+            <DialogContent>
+              <DialogContentText>
+                Please fill out the following fields.
+              </DialogContentText>
+              <TextField
+                // autoFocus
+                margin="dense"
+                id="date-input"
+                name="date"
+                label="Date"
+                type="date"
+                fullWidth
+                value={formValues.date}
+                onChange={handleInputChange}
+                InputLabelProps={{ shrink: true }}
+              />
+              <TextField
+                autoFocus
+                margin="dense"
+                id="activityName-input"
+                name="activityName"
+                label="Activity Name"
+                type="text"
+                fullWidth
+                value={formValues.activityName}
+                onChange={handleInputChange}
+                InputLabelProps={{ shrink: true }}
+              />
+              <TextField
+                autoFocus
+                margin="dense"
+                id="activityAddress-input"
+                name="activityAddress"
+                label="Activity Address"
+                type="address"
+                fullWidth
+                value={formValues.activityAddress}
+                onChange={handleInputChange}
+                InputLabelProps={{ shrink: true }}
+              />
+              {/* Add dropdown for type of activity */}
+              <TextField
+                autoFocus
+                margin="dense"
+                id="cost-input"
+                name="cost"
+                label="Cost"
+                type="number"
+                value={formValues.cost}
+                onChange={handleInputChange}
+                InputLabelProps={{ shrink: true }}
+              />
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleClose}>Cancel</Button>
+              <Button onClick={handleClose}>Add</Button>
+            </DialogActions>
+          </Dialog>
+        </div>
       </div>
     </div>
   );
