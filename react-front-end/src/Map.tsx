bbde9684536da1bcf55320c0e62746aecb187d7d
Lucy Shen <lucyshen77@gmail.com>
2022-03-20 23:48:01 -0700

    try to implement react-geocode package to generate coordinates from address. still need to address async issues.

--- react-front-end/src/Map.tsx
+++ react-front-end/src/Map.tsx
@@ -1,11 +1,15 @@
 import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
 import React from "react";
 import "./App.scss";
+import Geocode from "react-geocode";
+
+const mapAPIkey = process.env.REACT_APP_MAP_API_KEY;
+
+Geocode.setApiKey(`${mapAPIkey}`);
 
 export const Map: React.FC = () => {
-
   const { isLoaded, loadError } = useLoadScript({
-    googleMapsApiKey: process.env.REACT_APP_MAP_API_KEY as any
+    googleMapsApiKey: process.env.REACT_APP_MAP_API_KEY as any,
   });
 
   const containerStyle = {
@@ -16,15 +20,33 @@ export const Map: React.FC = () => {
     lat: 35.6938,
     lng: 139.7034,
   };
-  
+
+  // let obj = { lat: 0, lng: 0 };
+
+  // Geocode.fromAddress("Shinjuku Station").then(
+  //   (response) => {
+  //     const { lat, lng } = response.results[0].geometry.location;
+  //     obj.lat = lat;
+  //     obj.lng = lng;
+  //     console.log('obj inside', obj);
+  //     return obj;
+  //   },
+  //   (error) => {
+  //     console.error(error);
+  //   }
+  // );
+
   if (loadError) return <>'Error loading maps'</>;
   if (!isLoaded) return <>'Loading maps'</>;
 
   return (
     <div id="map">
       <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
-        <Marker position={{ lat: 35.6967, lng: 139.7056 }}></Marker> 
-        <Marker position={{ lat: 34.9671, lng: 135.7727 }}></Marker> 
+        <Marker position={{ lat: 35.6967, lng: 139.7056 }}></Marker>
+        <Marker position={{ lat: 34.9671, lng: 135.7727 }}></Marker>
+        {/* {obj.lat !== 0 && (
+          <Marker position={{ lat: obj.lat, lng: obj.lng }}></Marker>
+        )} */}
       </GoogleMap>
     </div>
   );
